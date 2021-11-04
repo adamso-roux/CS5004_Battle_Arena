3905e149b1f830d24145cd5303ed45cbd8c5d138
bartashb <90355740+bartashb@users.noreply.github.com>
2021-11-04 15:27:40 -0400

    Add files via upload

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Character implements CharacterInterface {
     private String name;
     private double hitPoints;
@@ -10,10 +12,15 @@ public class Character implements CharacterInterface {
         this.strength = strength;
     }
 
-
     //attacks based on the weapon blah blah blah
     @Override
     public double attack() {
-        return 0;
+        return (double)((Math.random() * (strength - 1)) + 1);
+    }
+
+    public String toString() {
+        return "Name: " + name
+                + "\nHit Points: " + hitPoints
+                + "\nStrength: " + strength;
     }
 }
