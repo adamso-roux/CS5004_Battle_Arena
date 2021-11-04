f59df94fe937c557e7d56d8b7196ba1da4dea6dd
derekgmuse <90358004+derekgmuse@users.noreply.github.com>
2021-11-04 16:33:21 -0400

    Add files via upload

---
+++ src/Ranged.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+
+public class Ranged extends Weapon{
+		private String name;
+	    private double strength;
+	    private int durability;
+
+	    public Ranged(String name, double strength, int durability){
+	        super(name, strength, durability);
+	    }
+
+	    @Override
+	    public String toString(){
+	        return super.toString();
+	    }
+	    
+	    public double attack(boolean special)
+	    {
+	    	if(this.durability == 0)
+	    	{
+	    		return 0;
+	    	}
+	    	
+	    	if(special)
+	    	{
+	    		this.setDurability(this.durability-1);
+	    		Random r1 = new Random();
+	    		double r = r1.nextDouble();
+	    		if(r>0.5)
+	    		{
+	    			return r*this.strength*2;
+	    		}
+	    		else{
+	    			return r*this.strength/2; 
+	    		}
+	    	}
+	    	else
+	    	{
+	    		this.setDurability(this.durability-1);
+	    		Random r1 = new Random();
+	    		return r1.nextDouble()*this.strength; 
+	    	}
+	    	
+	    }
+
+
+}
