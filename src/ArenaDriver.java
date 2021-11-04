251172ef54fcd12bccf0aec5aa92ba8298063197
Oliver Adams @ROUX <90351105+adamso-roux@users.noreply.github.com>
2021-11-04 16:34:10 -0400

    Add files via upload

--- src/ArenaDriver.java
+++ src/ArenaDriver.java
@@ -2,13 +2,40 @@ import java.util.LinkedList;
 
 public class ArenaDriver {
     public static void main(String[] args){
-        LinkedList<CharacterInterface> contestants = new LinkedList<CharacterInterface>();
+        //Create our list of heroes and bad guys:
+        LinkedList<CharacterInterface>  heroes = new LinkedList<CharacterInterface>();
+        LinkedList<CharacterInterface>  badguys = new LinkedList<CharacterInterface>();
 
+        //Populate our rack of weapons:
+        LinkedList<Weapon> weaponRack = ArenaDriver.getWeaponRack();
         Character oliver = new Character("Oliver", 0, 0);
-        Hero drG = new Hero("Dr.G", 1000, 1000000);
+        Hero drG = new Hero("Dr.G", 1000, 1000000, 10);
 
-        contestants.add(oliver);
-        contestants.add(drG);
+        //add some contestants:
+        heroes.add(oliver);
+        badguys.add(drG);
+
+        //The basic structure of the Arena function calls:
+        BattleArena ThunderDome = new BattleArena(heroes, badguys, weaponRack);
+        ThunderDome.AnnounceContestants();
+        ThunderDome.FIGHT();
+        ThunderDome.AnnounceWinner();
+
+
 
     }
+
+    public static LinkedList<Weapon> getWeaponRack() {
+        LinkedList<Weapon> weaponRack = new LinkedList<Weapon>();
+        weaponRack.add(new Weapon("a", 10));
+        weaponRack.add(new Weapon("b", 10));
+        weaponRack.add(new Weapon("c", 10));
+        weaponRack.add(new Weapon("d", 10));
+        weaponRack.add(new Weapon("e", 10));
+
+        return weaponRack;
+    }
 }
+
+
+
--- src/BattleArena.java
+++ src/BattleArena.java
@@ -1,28 +1,45 @@
 import java.util.LinkedList;
+import java.lang.Math;
 
 public class BattleArena {
     LinkedList<CharacterInterface> heroes;
     LinkedList<CharacterInterface> badguys;
+    LinkedList<Weapon> weaponRack;
 
-    public BattleArena(LinkedList<CharacterInterface> heroes, LinkedList<CharacterInterface> badguys){
+    public BattleArena(LinkedList<CharacterInterface> heroes,
+                       LinkedList<CharacterInterface> badguys,
+                       LinkedList<Weapon> weaponRack){
         this.heroes = heroes;
         this.badguys = badguys;
+        this.weaponRack = weaponRack;
     }
 
     public void FIGHT(){
-
+        //runs the contestants through the ringer, there will be only one winner
     }
 
-    /*
-    badguy : badGuyAttack
-    hero: attackWithWeapon
-
-
-     */
-    public double getRandomAttack(CharacterInterface contestant){
-
-        if
-
+    public void AnnounceContestants(){
+        //loops through the contestants in the arena.
     }
+
+    public void AnnounceWinner(){
+        //displays the winner and their weapon, fireworks, boom.
+    }
+    public double getRandomAttack(CharacterInterface contestant) {
+
+        if (contestant instanceof BadGuy) {
+            //attack function call for the badguy
+        }
+
+        if (contestant instanceof Hero) {
+            //hand of god roll:
+            int handofgod = (int) Math.floor(Math.random() * 100) % 2;
+
+            //attack function call for the hero
+
+        }
+        return 0d;
+    }
+
 
 }
