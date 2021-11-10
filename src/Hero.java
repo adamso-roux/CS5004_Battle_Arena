01d7b023be17b6f18e8e0b27c985fd019e844477
bartashb <90355740+bartashb@users.noreply.github.com>
2021-11-10 13:38:15 -0500

    Add files via upload

--- src/Hero.java
+++ src/Hero.java
@@ -9,6 +9,27 @@ public class Hero extends Character{
         this.mana = mana;
     }
 
+    // getters
+    public double getMana() {
+        return mana;
+    }
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    // setters
+    public void setMana(double mana) {
+        this.mana = mana;
+    }
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nMana: " + mana;
+    }
+
     // attack is based on the weapon
     public double attack(boolean special) {
         if(this.weapon.getDurability() == 0){
@@ -27,30 +48,4 @@ public class Hero extends Character{
         return this.weapon.attack();
     }
 
-    /*
-    public double specAttackWithWeapon(boolean special) {
-        return 0.0;
-    }
-     */
-
-    @Override
-    public String toString() {
-        return super.toString() + "\nMana: " + mana;
-    }
-
-    public double getMana() {
-        return mana;
-    }
-
-    public void setMana(double mana) {
-        this.mana = mana;
-    }
-
-    public Weapon getWeapon() {
-        return weapon;
-    }
-
-    public void setWeapon(Weapon weapon) {
-        this.weapon = weapon;
-    }
 }
