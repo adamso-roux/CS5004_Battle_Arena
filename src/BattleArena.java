05075efcbdca8f1d339d23fa4950a7caff1ffa8a
Oliver Adams @ROUX <90351105+adamso-roux@users.noreply.github.com>
2021-11-10 14:30:45 -0500

    Update BattleArena.java

--- src/BattleArena.java
+++ src/BattleArena.java
@@ -3,21 +3,49 @@ import java.lang.Math;
 
 
 public class BattleArena {
-    LinkedList<CharacterInterface> heroes;
-    LinkedList<CharacterInterface> badguys;
+    LinkedList<Character> heroes;
+    LinkedList<Character> badguys;
     LinkedList<Weapon> weaponRack;
     CharacterInterface winner;
 
-    public BattleArena(LinkedList<CharacterInterface> heroes,
-                       LinkedList<CharacterInterface> badguys,
+    public BattleArena(LinkedList<Character> heroes,
+                       LinkedList<Character> badguys,
                        LinkedList<Weapon> weaponRack){
         this.heroes = heroes;
         this.badguys = badguys;
         this.weaponRack = weaponRack;
     }
 
+
     public void FIGHT(){
-        //runs the contestants through the ringer, there will be only one winner
+
+        //LOOP WHILE size of heroes or badguys is greater than 0:
+
+        //get random index for a hero
+        //get random index for a bad guy
+        int hero_index = 0;// = random number from the size of hero linked list
+        int badguy_index = 0;// = random number from the size of hero linked list
+
+        int result = two_v_two_FIGHT(hero_index, badguy_index);
+
+        //the result is the index of the player who lost:
+        if(result == 0){this.heroes.remove(hero_index);}
+        if(result == 1){this.badguys.remove(badguy_index);}
+
+        //rest the health of the player who is still alive:
+
+
+
+    }
+
+    public int two_v_two_FIGHT(int hero_index, int badguy_index){
+
+
+        if(!this.heroes.get(hero_index).isAlive()) return 0;
+
+        else if(!this.badguys.get(badguy_index).isAlive()) return 1;
+
+        return -1;
     }
 
     public void AnnounceContestants(){
@@ -43,7 +71,6 @@ public class BattleArena {
             System.out.println(W);
         }
 
-
     }
 
     public void AnnounceWinner(){
@@ -60,6 +87,7 @@ public class BattleArena {
             //attack function call for the badguy
         }
 
+
         if (contestant instanceof Hero) {
             //hand of god roll:
             int handofgod = (int) Math.floor(Math.random() * 100) % 2;
@@ -72,4 +100,3 @@ public class BattleArena {
 
 
 }
-
