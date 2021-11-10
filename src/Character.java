98a6484afdc21282d890ca60ac72c6c32bdb6e32
bartashb <90355740+bartashb@users.noreply.github.com>
2021-11-10 13:29:45 -0500

    Add files via upload

--- src/Character.java
+++ src/Character.java
@@ -12,47 +12,61 @@ public class Character implements CharacterInterface {
         this.strength = strength;
     }
 
-    @Override
-    public double attack() {
-        return (double)((Math.random() * (strength - 1)) + 1);
-    }
-
-    public String toString() {
-        return "Name: " + name
-                + "\nHit Points: " + hitPoints
-                + "\nStrength: " + strength;
-    }
-
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getHitPoints() {
         return hitPoints;
     }
 
-    public void setHitPoints(double hitPoints) {
-        this.hitPoints = hitPoints;
-    }
-
     public double getStrength() {
         return strength;
-    }
-
-    public void setStrength(double strength) {
-        this.strength = strength;
     }
 
     public boolean isAlive() {
         return alive;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHitPoints(double hitPoints) {
+        if (hitPoints < 0) {
+            hitPoints=0;
+        }
+        if(hitPoints > 999) {
+            hitPoints = 999;
+        }
+        if (hitPoints == 0) {
+            alive = false;
+        }
+        this.hitPoints = hitPoints;
+    }
+
+    public void setStrength(double strength) {
+        this.strength = strength;
+    }
+
     public void setAlive(boolean alive) {
         this.alive = alive;
+    }
+
+    @Override
+    public double attack() {
+        return (double)((Math.random() * (strength - 1)) + 1);
+    }
+
+    public void takeDamage()
+    {
+        double damage = attack();
+        setHitPoints(hitPoints - damage);
+    }
+
+    public String toString() {
+        return "Name: " + name
+                + "\nHit Points: " + hitPoints
+                + "\nStrength: " + strength;
     }
 }
