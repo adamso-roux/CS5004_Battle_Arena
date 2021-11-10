215a7f37d8282d246f25d9b6ef466c7691b67a6b
derekgmuse <90358004+derekgmuse@users.noreply.github.com>
2021-11-10 13:39:19 -0500

    Updated Version

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,43 +1,44 @@
-public abstract class Weapon {
+/**
+ * Group: Oliver Adams, Bailee Bartash, Kriston Barnes, Derek Muse
+ * Abstract Weapon Class to reduce the reuse of code in each of the 
+ * three Weapon child classes.
+ */
 
-    private String name;
+public abstract class Weapon {
+		
+	//fields
+	private String name;
     private double strength;
     private int durability;
 
+    /**
+     * Constructor for a Weapon object
+     * @param name is the Weapon's name
+     * @param strength is the Weapon's strength between 0.0 and 25.0
+     * @param durability is the Weapon's durability between 0 and 10
+     */
     public Weapon(String name, double strength, int durability){
         this.name = name;
         this.strength = strength;
         this.durability = durability;
     }
 
-    public String getName()
-    {
-        return this.name;
-    }
-    public double getStrength()
-    {
-        return this.strength;
-    }
-    public int getDurability()
-    {
-        return this.durability;
-    }
+   /**
+    * Sets a Weapon's durability
+    * @param durability is the Weapon's new durability
+    */
+   public void setDurability(int durability)
+   {
+	   this.durability = durability;
+   }
+    
+    
 
-    public void setStrength(double strength)
-    {
-        this.strength = strength;
-    }
-    public void setDurability(int durability)
-    {
-        this.durability = durability;
-    }
-
-    public double attack(){
-        return 0d;
-    }
-
-
-    @Override
+   @Override
+   /**
+    * toString Override 
+    * @return the Weapon's characteristics in String format
+    */
     public String toString(){
         return String.format("[Weapon Name: %s, Strength: %d, Durability: %d]", this.name, this.strength, this.durability);
     }
