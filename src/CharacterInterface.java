92c94ee3d2d56b533b6781a1d1476ffee58428bd
bartashb <90355740+bartashb@users.noreply.github.com>
2021-11-15 16:49:10 -0500

    Update CharacterInterface.java

--- src/CharacterInterface.java
+++ src/CharacterInterface.java
@@ -1,6 +1,10 @@
+/**
+ * Group: Oliver Adams, Bailee Bartash, Kriston Barnes, Derek Muse
+ * setting up an interface here so that we can store our characters, heroes and bad
+ * guys all in a linked list
+ *
+ */
 
-//setting up an interface here so that we can store our characters, heroes and bad
-//guys all in a linked list
 public interface CharacterInterface {
     double attack();
 }
